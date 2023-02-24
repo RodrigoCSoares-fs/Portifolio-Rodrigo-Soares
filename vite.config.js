7efea6160c0206da5743d053fca36b34139fd406
Rodrigo Soares <rodrigo.soares0642@outlook.com.br>
2023-02-23 21:33:09 -0300

    set build path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://rodrigocsoares-fs.github.io/Portifolio-Rodrigo-Soares/',
   root: './',
         build: {
             outDir: './docs'
